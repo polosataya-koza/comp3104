469dda3f9569572806322d86edee6b8bc45cd1d8
Evgeniya <polosataya.koza@gmail.com>
2020-10-05 21:28:05 -0400

    adding our dependency lock file

---
+++ build/precache-manifest.4d558f73a46b07033ea0355d110cd0ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1fd98c633b39952bf12",
+    "url": "/polosataya-koza/comp3104/static/css/main.cbc33c4a.chunk.css"
+  },
+  {
+    "revision": "f1fd98c633b39952bf12",
+    "url": "/polosataya-koza/comp3104/static/js/main.f1fd98c6.chunk.js"
+  },
+  {
+    "revision": "eae0a19e3c4647b67de2",
+    "url": "/polosataya-koza/comp3104/static/js/runtime~main.eae0a19e.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/polosataya-koza/comp3104/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "3adb51dab46dfb5beff6a40b5d6e60a5",
+    "url": "/polosataya-koza/comp3104/index.html"
+  }
+];
