912a94943c69889399654765ccd30a0e22ec54f9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-19 22:52:48 +0000

    Deploy polosataya-koza/comp3104 to github.com/polosataya-koza/comp3104.git:gh-pages

---
+++ precache-manifest.9ec36d03f3ceb55dede9c09223e05ac4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "64b7b12a356051a2f676",
+    "url": "/polosataya-koza/comp3104/static/css/main.c195ea77.chunk.css"
+  },
+  {
+    "revision": "64b7b12a356051a2f676",
+    "url": "/polosataya-koza/comp3104/static/js/main.64b7b12a.chunk.js"
+  },
+  {
+    "revision": "eae0a19e3c4647b67de2",
+    "url": "/polosataya-koza/comp3104/static/js/runtime~main.eae0a19e.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/polosataya-koza/comp3104/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "93283df7753460c068369091d3f671bb",
+    "url": "/polosataya-koza/comp3104/index.html"
+  }
+];
